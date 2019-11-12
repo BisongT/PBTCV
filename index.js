cd7bdfbe98bb6b505e3c59fe4240852dec8f88db
BisongT <btangban@yahoo.co.uk>
2019-11-12 13:37:09 +1000

    Update index.js
    Added semicolon to the end of the code

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert("Hello, welcome to my site, I hope you enjoy your stay. Please click the OK button to close this alert")
+alert("Hello, welcome to my site, I hope you enjoy your stay. Please click the OK button to close this alert");
